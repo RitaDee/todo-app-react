066bfcd14a214f6efc1b5c25c7731dcb07dd545f
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-27 16:07:39 +0100

    Modify App.js file

---
+++ src/components/TodosLogic.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+// other imported components here
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+  return (
+    // ...
+  );
+};
+export default TodosLogic;
