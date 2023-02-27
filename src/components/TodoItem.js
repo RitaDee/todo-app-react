2c5657aa929f8cca3530df8e0d269c3e63d1ca3b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-27 19:03:46 +0100

    Add delete button to TOdoItem.js file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,15 @@
-const TodoItem = ({ itemProp, setTodos }) => {
-  const handleChange = (id) => {
-    setTodos((prevState) => prevState.map((todo) => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      }
-      return todo;
-    }));
-  };
+const TodoItem = ({ itemProp, handleChange }) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      <button>Delete</button>
+      {itemProp.title}
+    </li>
+  );
 };
 
 export default TodoItem;
