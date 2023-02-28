c6a820316f4845a0e3e242d8b83c7544fafd2f80
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-03-01 00:12:27 +0100

    Create and modify Login.module.css file

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from '@/styles/Login.module.css';
+import styles from '../styles/Login.module.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
