859a368ac876e1a85c4e114c61b2700216e66da9
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-28 08:51:37 +0100

    Trigger a submit event on form in the InputTodo.js file

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,20 +1,27 @@
- import { useState } from 'react';
+import { useState } from 'react';
+
 const InputTodo = () => {
   const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+    setTitle('');
+  };
 
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add Todo..."
         value={title}
         onChange={handleChange}
       />
-      <button>Submit</button>
+      <button type="button">Submit</button>
     </form>
   );
 };
