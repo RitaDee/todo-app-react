6383485b54eb430a4ea14174ab7226380e8be28f
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-28 10:41:12 +0100

    Modify TodoItem.js file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,29 @@
 import PropTypes from 'prop-types';
+import styles from '../styles/TodoItem.module.css';
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => (
-  <li>
-    <input
-      type="checkbox"
-      checked={itemProp.completed}
-      onChange={() => handleChange(itemProp.id)}
-    />
-    <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
-    {itemProp.title}
-  </li>
-);
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null} />
+        {itemProp.title}
+      </div>
+    </li>
+  );
+};
 
 TodoItem.propTypes = {
   itemProp: PropTypes.shape({
