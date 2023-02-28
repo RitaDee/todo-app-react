c1f1b652ea597730a411e3b54b1733428d0c12a0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-28 08:47:54 +0100

    Create a state for the user input in InputTodo.js file

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,7 +1,21 @@
-const InputTodo = () => (
-  <form>
-    <input type="text" placeholder="Add Todo..." />
-    <button type="button">Submit</button>
-  </form>
-);
+ import { useState } from 'react';
+const InputTodo = () => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        value={title}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+};
 export default InputTodo;
