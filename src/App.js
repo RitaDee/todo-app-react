8bf369991b465ce431a41ea5fc46d64a259d1f24
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-28 14:07:05 +0100

    Add dropdown feature to the Navbar.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import TodosLogic from './components/TodosLogic';
+import Navbar from './components/Navbar/Navbar';
 
 const TodoApp = () => (
   <div className="wrapper">
     <div className="todos">
       <Header />
+      <Navbar />
       <TodosLogic />
     </div>
   </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,48 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from 'react';
 
 const Navbar = () => {
-  console.log(useState(false));
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  document.title = `Current state value: ${dropdown}`;
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (
+        navbarOpen
+        && ref.current
+        && !ref.current.contains(event.target)
+      ) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [navbarOpen]);
+
   return (
-    <nav>
-      nav bar content
+    <nav ref={ref}>
+      <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>
+          <button type="button" onClick={() => setDropdown(!dropdown)}>
+            Services
+            {' '}
+            <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 };
